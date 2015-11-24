ba52ef13db8a67fc9524b7c1b7ed660be0fa3586
kuravih <kuravih@gmail.com>
2015-11-24 16:30:30 -0500

    more formatting (spelling)

--- common.h
+++ common.h
@@ -35,16 +35,16 @@ extern char * FILTER;
 extern char * LOCATION;
 
 struct header{
-    unsigned short xdim, ydim, day, year, xbin, ybin, num_subarrays, left, right, bottom1, top1, num_coadds;
-    float exp_time, wait_time; //seconds
-    float temperature;
-    char *site_name, *filename, *month, *filter;
+	unsigned short xdim, ydim, day, year, xbin, ybin, num_subarrays, left, right, bottom1, top1, num_coadds;
+	float exp_time, wait_time; //seconds
+	float temperature;
+	char *site_name, *filename, *month, *filter;
 };
 
 struct file_path{
-    char *folder_path, *filename, *full_path;
+	char *folder_path, *filename, *full_path;
 };
 
 struct config{
-    char *config_site, *config_cam, *config_path;
+	char *config_site, *config_cam, *config_path;
 };
