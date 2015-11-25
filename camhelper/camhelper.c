92bab3f516af23144d079cb8acbf7bbbd2b01be7
kuravih <kuravih@gmail.com>
2015-11-25 15:13:39 -0500

    had to make a copy of the image array to write to file

--- camhelper/camhelper.c
+++ camhelper/camhelper.c
@@ -5,7 +5,6 @@
 extern int init_camera();
 extern int uninit_camera();
 extern char * capture(long exp_time);
-extern char * write_file(struct data data_obj, char *location);
 extern char * preview(long exp_time_ms, int sock);
 
 extern int init_camera(){
@@ -19,82 +18,42 @@ extern int uninit_camera(){
     return 0;
 }
 
-extern char * write_file(struct data data_obj, char *location){
-	int status;
-	long nelements = data_obj.xdim*data_obj.ydim;
-	long naxis = 2;
-	long naxes[2] = {data_obj.xdim,data_obj.ydim};   /* image is 300 pixels wide by 200 rows */
-	long fpixel = 1;
-
-	struct stat st = {0};
-	if (stat(location, &st) == -1) mkdir(location, 0700);
-
-	char * full_path = (char *)malloc( strlen(location)+strlen(data_obj.filename)+2);
-	sprintf(full_path, "%s/%s", location, data_obj.filename);
-
-	fitsfile *fptr;       /* pointer to the FITS file; defined in fitsio.h */
-
-	status = 0;
-	fits_create_file(&fptr, full_path, &status);   /* create new file */
-	fits_create_img(fptr, SHORT_IMG, naxis, naxes, &status);
-
-	fits_update_key(fptr, TLONG, "EXPOSURE", &data_obj.exp_time_ms, "Total Exposure Time", &status);
-	fits_write_key_unit(fptr, "EXPOSURE", "ms", &status);
-
-	fits_update_key(fptr, TFLOAT, "TEMPERATURE", &data_obj.temperature, "Camera Temperature", &status);
-	fits_write_key_unit(fptr, "TEMPERATURE", "C", &status);
-
-	fits_update_key(fptr, TSTRING, "SITE", data_obj.site, "Instrument Location", &status);
-
-	fits_update_key(fptr, TSTRING, "CAMERA", data_obj.camera, "Instrument Camera", &status);
-
-	fits_write_date(fptr, &status);
-
-	/* Write the array of integers to the image */
-	fits_write_img(fptr, TSHORT, fpixel, nelements, data_obj.image[0], &status);
-
-	fits_close_file(fptr, &status);            /* close the file */
-
-	fits_report_error(stderr, status);  /* print out any error messages */
-	return full_path;
-}
-
 extern char * capture(long exp_time_ms){
 
-	struct data data_obj;
+	struct data data_object;
 
-	data_obj.xdim = 128;
-	data_obj.ydim = 128;
-	data_obj.xbin = 1;
-	data_obj.ybin = 1;
-	data_obj.num_subarrays = 1;
-	data_obj.left = 0;
-	data_obj.right = 1024;
-	data_obj.bottom1 = 0;
-	data_obj.top1 = 1024;
-	data_obj.num_coadds = 1;
-	data_obj.exp_time_ms = exp_time_ms;
-	data_obj.temperature = -60.0;
+	data_object.xdim = 32;
+	data_object.ydim = 32;
+	data_object.xbin = 1;
+	data_object.ybin = 1;
+	data_object.num_subarrays = 1;
+	data_object.left = 0;
+	data_object.right = 1024;
+	data_object.bottom1 = 0;
+	data_object.top1 = 1024;
+	data_object.num_coadds = 1;
+	data_object.exp_time_ms = exp_time_ms;
+	data_object.temperature = -60.0;
 
-	data_obj.image = dyanmically_allocate(data_obj.xdim,data_obj.xdim,sizeof(short*));
+	data_object.image = dyanmically_allocate(data_object.xdim,data_object.xdim,sizeof(short*));
 
 	short ii, jj;
-	for (jj = 0; jj < data_obj.xdim; jj++)
-		for (ii = 0; ii < data_obj.ydim; ii++)
-			data_obj.image[jj][ii] = ii + jj;
+	for (jj = 0; jj < data_object.xdim; jj++)
+		for (ii = 0; ii < data_object.ydim; ii++)
+			data_object.image[ii][jj] = ii+jj;
 
 	struct config config_obj = read_config();
 	struct file_path file_path_obj = file_path_string();
 
-	data_obj.filename = file_path_obj.filename;
-	data_obj.site = config_obj.site;
-	data_obj.camera = config_obj.camera;
+	data_object.filename = file_path_obj.filename;
+	data_object.site = config_obj.site;
+	data_object.camera = config_obj.camera;
 
-	sleep(ceil(data_obj.exp_time_ms/1000));
+	sleep(ceil(data_object.exp_time_ms/1000));
 
 	char * location = (char *)malloc(strlen(config_obj.path)+strlen(file_path_obj.folder_name)+2);
 	sprintf(location, "%s/%s", config_obj.path, file_path_obj.folder_name);
-	return write_file(data_obj, location);
+	return write_file(data_object, location);
 }
 
 
--- common.c
+++ common.c
@@ -6,10 +6,11 @@ extern void copy_header(struct header header_struct, unsigned char *ptr_filehead
 extern struct file_path file_path_string();
 extern struct config read_config();
 extern short ** dyanmically_allocate(short xdim, short ydim, int type_size);
+extern char * write_file(struct data data_object, char *location);
 
 extern struct config read_config(){
 
-	struct config config_obj;
+	struct config config_object;
 
 	FILE *cfg_file;
 	cfg_file = fopen("config.cfg" , "r");
@@ -26,44 +27,89 @@ extern struct config read_config(){
 		parameter_str = strtok(line, "=");
 		val_str = strtok(NULL, "\n");
 		if (strcmp(parameter_str, "SITE") == 0){
-			config_obj.site = (char *)malloc(strlen(val_str));
-			strcpy(config_obj.site, val_str);
+			config_object.site = (char *)malloc(strlen(val_str));
+			strcpy(config_object.site, val_str);
 		} else if (strcmp(parameter_str, "CAMERA") == 0){
-			config_obj.camera = (char *)malloc(strlen(val_str));
-			strcpy(config_obj.camera, val_str);
+			config_object.camera = (char *)malloc(strlen(val_str));
+			strcpy(config_object.camera, val_str);
 		} else if (strcmp(parameter_str, "PATH") == 0){
-			config_obj.path = (char *)malloc(strlen(val_str));
-			strcpy(config_obj.path, val_str);
+			config_object.path = (char *)malloc(strlen(val_str));
+			strcpy(config_object.path, val_str);
 		}
 	}
 	fclose(cfg_file);
-	return config_obj;
+	return config_object;
 }
 
 extern struct file_path file_path_string() {
 
-	struct file_path file_path_obj;
+	struct file_path file_path_object;
 
 	time_t raw_time;
 	struct tm *time_info;
 	time(&raw_time);
 	time_info = localtime(&raw_time);
 
-	file_path_obj.filename = (char *)malloc(18);
-	strftime(file_path_obj.filename, 18,"img%H%M%S_%j.fits", time_info);
+	file_path_object.filename = (char *)malloc(18);
+	strftime(file_path_object.filename, 18,"img%H%M%S_%j.fits", time_info);
 
-	file_path_obj.folder_name = (char *)malloc(8);
-	strftime(file_path_obj.folder_name,8,"%b%d%y", time_info);
+	file_path_object.folder_name = (char *)malloc(8);
+	strftime(file_path_object.folder_name,8,"%b%d%y", time_info);
 
-	return file_path_obj;
+	return file_path_object;
 }
 
 extern short ** dyanmically_allocate(short xdim, short ydim, int type_size){
 	short **image;
 	image = (short **)malloc(xdim*type_size);
 	short i;
-	for (i = 0; i < xdim; i++) {
+	for (i = 0; i < xdim; i++)
 		image[i] = malloc(ydim*type_size);
-	}
 	return image;
+}
+
+extern char * write_file(struct data data_object, char *location){
+	int status;
+	long nelements = data_object.xdim*data_object.ydim;
+	long naxis = 2;
+	long naxes[2] = {data_object.xdim,data_object.ydim};   /* image is 300 pixels wide by 200 rows */
+	long fpixel = 1;
+
+	struct stat st = {0};
+	if (stat(location, &st) == -1) mkdir(location, 0700);
+
+	char * full_path = (char *)malloc( strlen(location)+strlen(data_object.filename)+1);
+	sprintf(full_path, "%s/%s", location, data_object.filename);
+
+	fitsfile *fptr;       /* pointer to the FITS file; defined in fitsio.h */
+
+	status = 0;
+	fits_create_file(&fptr, full_path, &status);   /* create new file */
+	fits_create_img(fptr, SHORT_IMG, naxis, naxes, &status);
+
+	fits_update_key(fptr, TLONG, "EXPOSURE", &data_object.exp_time_ms, "Total Exposure Time", &status);
+	fits_write_key_unit(fptr, "EXPOSURE", "ms", &status);
+
+	fits_update_key(fptr, TFLOAT, "TEMPERATURE", &data_object.temperature, "Camera Temperature", &status);
+	fits_write_key_unit(fptr, "TEMPERATURE", "C", &status);
+
+	fits_update_key(fptr, TSTRING, "SITE", data_object.site, "Instrument Location", &status);
+
+	fits_update_key(fptr, TSTRING, "CAMERA", data_object.camera, "Instrument Camera", &status);
+
+	fits_write_date(fptr, &status);
+
+	short ii, jj;
+	short copy[data_object.xdim][data_object.ydim];
+	for (jj = 0; jj < data_object.xdim; jj++)
+		for (ii = 0; ii < data_object.ydim; ii++)
+			copy[ii][jj] = data_object.image[ii][jj];
+
+	/* Write the array of integers to the image */
+	fits_write_img(fptr, TSHORT, fpixel, nelements, copy[0], &status);
+
+	fits_close_file(fptr, &status);            /* close the file */
+
+	fits_report_error(stderr, status);  /* print out any error messages */
+	return full_path;
 }
