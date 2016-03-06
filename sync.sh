2270ea16553e24f0e1a737d6d4516983c73168dd
Kuravi Hewawasam <kuravih@gmail.com>
2016-03-05 20:43:07 -0500

    time syncing implemented, and camera type and user are different parameters now

--- sync.sh
+++ sync.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
 uncomitted=$(git status -s)
 # some arguments don't have a corresponding value to go with it such as in the --default example.
+# ./sync.sh --client=192.168.1.2 --user=ikon --camera==pixis -f --time
 for i in "$@"
 do
 case $i in
 
-	-ip=*|--client=*)
+	-ip=*|--client=*) # the ip of the clent
 		ip="${i#*=}"
 		shift # past argument=value
 	;;
 
-	-u=*|--user=*)
+	-u=*|--user=*) # the user on the client to run camdaemon
 		user="${i#*=}"
 		shift # past argument=value
 	;;
 
-	-c=*|--camera=*)
+	-c=*|--camera=*) # camera type
 		camera="${i#*=}"
+		code=true
 		shift # past argument=value
 	;;
 
 	-f|--force) # force push even if uncomitted changes available
 		force=true
+		shift # past argument=value
+	;;
+
+	-t|--time) # force push even if uncomitted changes available
+		time=true
 		shift # past argument=value
 	;;
 
@@ -37,22 +44,54 @@ case $i in
 esac
 done
 
-if [[ -z "$uncomitted" ]] || [ "$force" = true ] ; then
-	echo 'pushing git revision '$(git rev-parse HEAD)' to server'
+if [ "$time" = true ]; then
 
-	if [ -z "$ip" ] || [ -z "$camera" ]; then
+	if [ -z "$ip" ] || [ -z "$user" ]; then
 		echo 'Requires both ip and camera'
 	else
-		echo 'rsyncing files to '"$user"@"$ip"
-		rsync --exclude='data/*' --exclude='LICENSE' --exclude='README.md'  --exclude='camdaemon' --exclude='sync.sh' -a -v * "$user"@"$ip":~/camdaemon
-		echo 'rsyncing complete'
-		echo 'compiling camdaemon for '"$camera"
-		ssh "$user"@"$ip" 'make '$camera' -C ~/camdaemon'
+		echo setting server time to client time
+		# Sun Mar  6 01:34:35 CET 2016
+		clienttime="$(date +'"%d %b %Y %T"')"
+		echo "$clienttime"
+		ssh "$user"@"$ip" "sudo date --set=$clienttime"
 	fi
 
-else
-	echo 'uncomitted changes available'
-	echo "${uncomitted}"
-	echo 'commit changes before pushing to the server'
-	exit
+fi
+
+if [ "$code" = true ]; then
+
+	if [[ -z "$uncomitted" ]] || [ "$force" = true ]; then
+
+		echo pushing git revision "$(git rev-parse HEAD)" to server
+
+		if [ -z "$ip" ] || [ -z "$user" ]; then
+			echo 'Requires both ip and user'
+		else
+			echo rsyncing files to "$user"@"$ip"
+			rsync --exclude='data/*' --exclude='LICENSE' --exclude='README.md'  --exclude='camdaemon' --exclude='sync.sh' --exclude='daemon.sh' -a -v * "$user"@"$ip":/home/"$user"/camdaemon
+			echo rsyncing complete
+			echo compiling camdaemon for "$camera" on "$user"@"$ip"
+			ssh "$user"@"$ip" "make "$camera" -C /home/"$user"/camdaemon"
+
+			daemonsh='#!/bin/bash
+/bin/su - '"$user"' -c "cd /home/'"$user"'/camdaemon; ./camdaemon -p 3000"'
+
+			echo "$daemonsh" | ssh "$user"@"$ip" "cat > /home/"$user"/camdaemon/daemon.sh"
+
+			ssh "$user"@"$ip" "chmod +x /home/"$user"/camdaemon/daemon.sh"
+
+		fi
+
+		exit
+
+	else
+
+		echo 'uncomitted changes available'
+		echo "${uncomitted}"
+		echo 'commit changes before pushing to the server'
+		exit
+
+	fi
+
+
 fi
